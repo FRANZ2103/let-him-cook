f40e7c77ce3228af8d34274c1d1225acb5d50a82
FRANZ2103 <96021965+FRANZ2103@users.noreply.github.com>
2026-06-13 08:06:19 +0800

    Added mobile support for header

--- chef components/components/ChefHeader.jsx
+++ chef components/components/ChefHeader.jsx
@@ -21,21 +21,23 @@ export default function ChefHeader() {
     setDarkMode((prevMode) => !prevMode);
   }
   return (
-    <nav className="dg-nav bg-[#fafaf8] dark:bg-[#19191C] dark:text-gray-200 flex justify-between fixed top-0 left-0 min-w-full min-h-[4rem]">
-      <div className="dg-logo flex items-center pl-4">
+    <nav className="bg-[#fafaf8] dark:bg-[#19191C] dark:text-gray-200 flex justify-between items-center fixed top-0 left-0 w-full min-h-[4rem] px-3 md:px-6">
+      <div className="flex items-center gap-2">
         <img
-          className="dg-logo-icon dark:text-gray-200"
+          className="w-7 h-7 md:w-9 md:h-9"
           src={darkMode ? chefLogo2 : chefLogo}
           alt="logo"
           onMouseOver={handleMouseHover}
         />
-        <span className="text-xl dark:text-[#FF6B6B]">Let Him Cook!</span>
+        <span className="text-sm md:text-xl dark:text-[#FF6B6B] font-medium">
+          Let Him Cook!
+        </span>
       </div>
-      <div className="flex justify-end items-center gap-4 max-h-[1.5rem]">
-        <span className="inline-flex items-center rounded-xl bg-[#f0efe8] px-2 py-1 text-xs font-medium text-[#888780] inset-ring inset-ring-gray-500/10">
+
+      <div className="flex items-center gap-2 md:gap-4">
+        <span className="inline-flex items-center rounded-xl bg-[#f0efe8] dark:bg-[#2E2E2B] px-2 py-1 text-xs font-medium text-[#888780]">
           Beta
         </span>
-
         <DarkModeToggle onToggle={toggleDarkMode} />
       </div>
     </nav>
